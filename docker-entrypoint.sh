013e6096c9c05cff002fe6c67098cd192cca035a
Ash Hatef <ash@begradient.com>
2026-02-23 16:29:04 +0100

    fix: remove broken migration from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Run database migrations
-echo "Running database migrations..."
-node node_modules/prisma/build/index.js db push --accept-data-loss --skip-generate
-
 # Start the application
 echo "Starting application..."
 exec node server.js
